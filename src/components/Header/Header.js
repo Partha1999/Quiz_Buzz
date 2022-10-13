24744b3d579e46619539008e8f000f913104a58d
Partha1999 <impartha1999@gmail.com>
2022-10-14 01:00:44 +0600

    header design updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <Link to="#" className="flex items-center justify-center">
             <img className="w-8 h-8 mr-4" src={img2} alt="" />
-            <span className="self-center text-xl font-bold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">
               QuizBuzz
             </span>
           </Link>
@@ -43,7 +43,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/"
-                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
+                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent text-xl"
                   aria-current="page"
                 >
                   Home
@@ -52,7 +52,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/Topics"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
                 >
                   Topics
                 </Link>
@@ -60,7 +60,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/Statistics"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
                 >
                   Statistics
                 </Link>
@@ -68,7 +68,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/Blog"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
                 >
                   Blog
                 </Link>
